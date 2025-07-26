08038ef19942f5979bda0cf52d5e6f6b35de5270
veneruzzo <34711248+veneruzzo@users.noreply.github.com>
2025-07-26 20:33:51 +0200

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Cavamalusa is alive ⚡
+      </h1>
+    </div>
+  );
+}
